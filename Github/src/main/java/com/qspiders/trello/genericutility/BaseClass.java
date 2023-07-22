8400f2b8015099ce9fdef6b6bdaa0598028791aa
HP <HP@DESKTOP-3DIM7HF>
2023-07-22 13:03:03 +0530

    3rd comment

--- Github/src/main/java/com/qspiders/trello/genericutility/BaseClass.java
+++ Github/src/main/java/com/qspiders/trello/genericutility/BaseClass.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
+@fjfb
 public class BaseClass {
 	public WebDriver driver;
 	public FileUtility fileUtils = new FileUtility();
